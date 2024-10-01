59da0a90cfdc0d521dd6d047c7bc78cc51120b98
kajal-jotwani <kajaljotwani06@gmail.com>
2024-10-02 00:43:27 +0530

    removed unnecessary divs

--- src/app/page.js
+++ src/app/page.js
@@ -150,114 +150,106 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-800 to-black p-8">
       <h1 className="text-5xl font-bold text-white mb-10 font-['Roboto']">Codeforces Submission Scraper</h1>
-      <div className="flex flex-col items-center w-full max-w-2xl space-y-8">
-        <div className="bg-white shadow-xl rounded-lg p-8 w-full"> 
-          <h2 className="text-2xl font-semibold mb-6">Submit Problem Details</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="flex flex-col">
-            <input
-              type="text"
-              value={contestID}
-              onChange={(e) => setContestID(e.target.value)}
-              placeholder="Enter Contest ID"
-              className="h-[6vh] px-4 py-3 text-black border-2 border-gray-300 rounded-lg mb-4 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-            <input
-              type="text"
-              value={problemIndex}
-              onChange={(e) => setProblemIndex(e.target.value)}
-              placeholder="Enter Problem Index"
-              className="h-[6vh] px-4 py-3 text-black border-2 border-gray-300 rounded-lg mb-4 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              className="px-4 py-2 text-black bg-white border-2 border-gray-300 rounded-lg mb-4 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            >
-            <option value="">Select Category</option>
-            {categories.map((cat) => (
-              <option key={cat} value={cat}>
-                {cat.charAt(0).toUpperCase() + cat.slice(1)}
-              </option>
-            ))}
-            </select>
-            <select
-              value={language}
-              onChange={(e) => setLanguage(e.target.value)}
-              className="px-4 py-3 text-black bg-white border-2 border-gray-300 rounded-lg mb-4 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            >
-            <option value="">Select Language</option>
-            {languages.map((lang) => (
-              <option key={lang} value={lang}>
-                {lang}
-              </option>
-            ))}
-            </select>
-            <button
-              type="submit"
-              className="bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out"
-              disabled={loading}
-            >
-              {loading ? "Scraping..." : "Scrape Submission"}
-            </button>
-          </div>
-        </form>
-        {error && <p className="mt-4 text-red-400">{error}</p>}
-        </div>
+      <form onSubmit={handleSubmit} className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-semibold text-gray-900 mb-6">Submit Problem Details</h2>
 
-        {result && (
-          <div className="bg-white shadow-xl rounded-lg p-8 w-full">
-           <h2 className="text-2xl font-semibold mb-4">Results:</h2>
-           <div className="space-y-2">
-            <pre className="bg-gray-100 p-2 rounded-lg overflow-x-auto border-2 border-gray-800 shadow-md">
-              {result.length > 0 ? (
-               <div>
-                  <div className="mb-2">
-                    <strong>Author:</strong> {result[0].author}
-                  </div>
-                  <div className="mb-2">
-                    <a
-                      href={result[0].submission} // Adjust URL as needed
-                      target="_blank"
-                      rel="noreferrer"
-                      className="text-blue-600 underline"
-                    >
-                      View Submission
-                   </a>
-                  </div>
-                  <div className="mb-2">
-                    <button
-                      onClick={handleExplainClick}
-                     className="text-blue-600 underline bg-transparent border-none cursor-pointer"
-                    >
-                     View the Code
-                    </button>
-                 </div>
+        <input
+          type="text"
+          value={contestID}
+          onChange={(e) => setContestID(e.target.value)}
+          placeholder="Enter Contest ID"
+          className="w-full h-12 px-4 py-2 text-black border border-gray-300 rounded-lg mb-4"
+          required
+        />
+        <input
+          type="text"
+          value={problemIndex}
+          onChange={(e) => setProblemIndex(e.target.value)}
+          placeholder="Enter Problem Index"
+          className="w-full h-12 px-4 py-2 text-black border border-gray-300 rounded-lg mb-4"
+          required
+        />
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="w-full h-12 px-4 py-2 pr-8 text-black border border-gray-300 rounded-lg mb-4 "
+          required
+        >
+          <option value="">Select Category</option>
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat.charAt(0).toUpperCase() + cat.slice(1)}
+            </option>
+          ))}
+        </select>
+        <select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+          className="w-full h-12 px-4 py-2 pr-8 text-black border border-gray-300 rounded-lg mb-4"
+          required
+        >
+          <option value="">Select Language</option>
+          {languages.map((lang) => (
+            <option key={lang} value={lang}>
+              {lang}
+            </option>
+          ))}
+        </select>
+        <button
+          type="submit"
+          className="w-full font-semibold bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600"
+          disabled={loading}
+        >
+          {loading ? "Scraping..." : "Scrape Submission"}
+        </button>
+      </form>
+      {error && <p className="mt-4 text-red-500">{error}</p>}
+      {result && (
+        <div className="mt-4 w-full max-w-md bg-black p-4 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-2 text-white">Results:</h2>
+          <pre className="bg-gray-800 p-2 rounded-lg overflow-x-auto text-white">
+            {result.length > 0 ? (
+              <div>
+                <div className="mb-2">
+                  <strong>Author:</strong> {result[0].author}
                 </div>
-              ) : (
+                <div className="mb-2">
+                  <a
+                    href={result[0].submission} // Adjust URL as needed
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 underline"
+                  >
+                    View Submission
+                  </a>
+                </div>
+                <div className="mb-2">
+                  <button
+                    onClick={handleExplainClick}
+                    className="text-blue-500 underline bg-transparent border-none cursor-pointer"
+                  >
+                    View the Code
+                  </button>
+                </div>
+              </div>
+            ) : (
               <p>No results found.</p>
             )}
           </pre>
         </div>
-      </div>
       )}
-      
       {sourceCode && (
         <div>
-          <div className="bg-white shadow-xl rounded-lg p-8 w-full">
-            <h2 className="text-2xl font-semibold mb-4">Source Code:</h2>
-            <pre className="bg-gray-100 p-2 rounded-lg overflow-x-auto border-2 border-gray-800 shadow-md">
+          <div className="mt-4 w-full max-w-md bg-black p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold mb-2 text-white">Source Code:</h2>
+            <pre className="bg-gray-800 p-2 rounded-lg overflow-x-auto text-white">
               {sourceCode}
             </pre>
           </div>
           <div>
             <button
               onClick={maneesh}
-              className="bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out mt-4 w-full"
+              className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
             >
               Explain
             </button>
@@ -265,14 +257,13 @@ export default function Home() {
         </div>
       )}
       {explanation && (
-        <div className="bg-white shadow-xl rounded-lg p-8 w-full">
-          <h2 className="text-2xl font-semibold mb-4">Explanation:</h2>
-          <pre className="bg-gray-100 p-2 rounded-lg overflow-x-auto border-2 border-gray-800 shadow-md">
+        <div className="mt-4 w-full max-w-md bg-black p-4 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-2 text-white">Explanation:</h2>
+          <pre className="bg-gray-800 p-2 rounded-lg overflow-x-auto text-white">
             {explanation}
           </pre>
         </div>
       )}
-    </div> 
-  </div>
+    </div>
   );
 }
