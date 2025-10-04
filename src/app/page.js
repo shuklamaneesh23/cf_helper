690080f1790503ba91e5304bd72f1c0d59899260
Ashnidh <ashnidh@gmail.com>
2025-10-04 14:03:20 +0530

    Added support for newer languages.

--- src/app/page.js
+++ src/app/page.js
@@ -31,10 +31,11 @@ export default function Home() {
   ];
 
   const languageCategories = {
-    PyPy: ["PyPy 3-64", "PyPy 3"],
-    "C++": ["C++17 (GCC 7-32)", "C++14 (GCC 6-32)", "C++20 (GCC 13-64)"],
-    Python: ["Python 3"],
-    Java: ["Java 21"],
+    C: ["GNU C11"],
+    "Python/PyPy": ["Python 2", "Python 3", "PyPy 2", "PyPy 3", "PyPy 3-64"],
+    "C++": ["C++14 (GCC 6-32)", "C++17 (GCC 7-32)", "C++20 (GCC 13-64)", "C++23 (GCC 14-64, msys2)"],
+    Java: ["Java 6", "Java 7", "Java 8", "Java 21"],
+    Kotlin: ["Kotlin 1.7", "Kotlin 1.9", "Kotlin 2.2"],
     JavaScript: ["JavaScript"]
   };
 
